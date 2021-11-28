90987fd2dcd34f66b8c5ec7a34b9ed6cc8afc716
patricuismart <patricuismart@gmail.com>
2021-11-28 18:23:43 +0100

    Add login functionality

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,5 +1,25 @@
 'use strict';
+const user = document.querySelector('.js_user');
+const pass = document.querySelector('.js_pass');
+const button = document.querySelector('.js_buton');
 
-console.log('hola');
+let results = [];
+
+//User Login
+
+function userLogin() {
+  if (user.value === 'CEO' && pass.value === '1234') {
+    console.log('im in');
+  } else {
+    alert('Porfavor ingrese nombre de usuario y contraseña correctos.');
+  }
+}
+// Handle Functions
+
+function handleButton() {
+  userLogin();
+}
+//Events
+button.addEventListener('click', handleButton);
 
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,23 @@
 'use strict';
+const user = document.querySelector('.js_user');
+const pass = document.querySelector('.js_pass');
+const button = document.querySelector('.js_buton');
 
-console.log('hola');
+let results = [];
+
+//User Login
+
+function userLogin() {
+  if (user.value === 'CEO' && pass.value === '1234') {
+    console.log('im in');
+  } else {
+    alert('Porfavor ingrese nombre de usuario y contraseña correctos.');
+  }
+}
+// Handle Functions
+
+function handleButton() {
+  userLogin();
+}
+//Events
+button.addEventListener('click', handleButton);
