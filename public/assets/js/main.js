35a908a41a80a4e23ce0f8094a6b6d89fe9d4fec
patricuismart <patricuismart@gmail.com>
2021-12-04 13:21:25 +0100

    Add hide section when login

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,23 +1,31 @@
 'use strict';
+
+// global variables
 const user = document.querySelector('.js_user');
 const pass = document.querySelector('.js_pass');
 const button = document.querySelector('.js_buton');
+const hideSection = document.getElementById('hideSection');
 const resultsContainer = document.querySelector('.js_results');
+const pagination = document.querySelector('.js_pagination');
 
 // info array results api json
 let login = [];
 let results = [];
 let html = '';
+let buttons = '';
 
 //User Login
-
 function userLogin() {
   let currentList = '';
   if (user.value === login.user && pass.value === login.pass) {
     console.log('im in');
+
+    // go through array of objects
     for (let index = 0; index < results.length; index++) {
       const malware = results[index];
-      currentList += renderMalwareItem(malware);
+      currentList = renderMalwareItem(malware);
+      hideSection.classList.add('hidden');
+      console.log('malware', malware);
     }
   } else {
     alert('Porfavor ingrese nombre de usuario y contraseña correctos.');
@@ -26,26 +34,13 @@ function userLogin() {
 }
 
 //Render List of results
-
 function renderMalwareItem(malware) {
   html += `  <h2 class="results__tittle">resultados</h2>
-       <li key={index} className="results__item">
-          <p className="results__name">Nombre del fichero: ${malware.name}</p>
-          <p className="results__date">
-            <label className="results__label">
-              fecha de inserción: ${malware.date}
-            </label>
-          </p>
-          <p className="results__actualization">
-            <label className="results__label">
-              última actualización: ${malware.actualization}
-            </label>
-          </p>
-          <p className="results__os">
-            <label className="results__label">
-              sistema operativo: ${malware.os}
-            </label>
-          </p>
+       <li class="results__item">
+          <p class="results__name">Nombre del fichero: ${malware.name}</p>
+          <p class="results__date">fecha de inserción: ${malware.date}</p>
+          <p class="results__actualization">última actualización: ${malware.actualization}</p>
+          <p class="results__os">sistema operativo: ${malware.os}</p>
         </li>`;
   return html;
 }
@@ -56,6 +51,8 @@ function handleButton(event) {
   event.preventDefault();
   userLogin();
 }
+
+//Pagination
 
 //Call to api
 // FETCH
@@ -68,7 +65,6 @@ function callToApi() {
       login = data.login;
       results = data.results;
     });
-  console.log(results);
 }
 
 callToApi();
